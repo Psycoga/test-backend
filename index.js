5840c20b9ad1caff7cb9674c46ff3024d4657dd2
Psycoga <davidgarrigu@gmail.com>
2025-01-23 12:23:52 -0500

    test-backend

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
+const morgan = require('morgan'); // Importa Morgan
 const db = require('./initdb');
 const geoip = require('geoip-lite');
 const port = 3000;
 const app = express();
 
+// Usa el middleware de Morgan con el formato 'dev'
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
     res.send("Hola mundo");
- });
-
+});
 
 app.get("/usuarios", (req, res) => {
     const stmt = "SELECT * FROM usuarios";
     const usuarios = db.prepare(stmt).all();
     res.json(usuarios);
-    });
+});
 
-
-app.get('/imagenes', (req,res) => {
-
+app.get('/imagenes', (req, res) => {
     const ip = req.ip;
     const userAgent = req.get('userAgent');
     const fecha = new Date().toISOString();
-    const localizacion = geoip.lookup(ip).city;
+    const localizacion = geoip.lookup(ip)?.city || 'Desconocido'; // Manejo de localización desconocida
     const insert = db.prepare("INSERT INTO usuarios (ip, userAgent, localizacion, fecha) VALUES (?,?,?,?)");
     insert.run(ip, userAgent, localizacion, fecha);
 
@@ -31,16 +32,15 @@ app.get('/imagenes', (req,res) => {
         "3.jpg",
         "4.jpg",
         "5.jpg",
-    ]
+    ];
 
     const numeroAleatorio = Math.floor(Math.random() * imagenes.length);
-    console.log(numeroAleatorio);
     const imagen = imagenes[numeroAleatorio];
-
 
     res.sendFile(__dirname + `/${imagen}`);
 });
 
+// Inicia el servidor
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
